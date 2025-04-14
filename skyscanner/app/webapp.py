f540a22b19b6ca6191a1b195b0ec5cf7504757d8
rishabhnair12345 <rishabhnair12345@gmail.com>
2025-04-14 13:13:41 +0530

    enabled Smtp and added new features

--- skyscanner/app/webapp.py
+++ skyscanner/app/webapp.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from flask_mail import Mail, Message
 import os
 from flights import get_flight_prices, process_flight_data
 from config_loader import get_airport_code, get_location_id
@@ -6,6 +7,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
+mail=Mail(app)
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = os.getenv('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.getenv('EMAIL_PASS')
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -15,11 +25,10 @@ def index():
         date = request.form['date']
         destination_id=get_location_id(destination)
         origin_id=get_location_id(origin)
+        email_id = request.form['email']
         api_key = request.form['api_key'] or os.getenv("RAPIDAPI_KEY")
 
-        if not api_key:
-            return render_template('index.html', error="API Key is required.")
-
+      
         flights = get_flight_prices(
         api_key=api_key,
         date=date,
@@ -28,6 +37,12 @@ def index():
         destination=destination,
         destination_id=destination_id)
         results = process_flight_data(flights, origin, destination, date)
+        msg=Message('Flight Search Results', sender=os.getenv('EMAIL_USER'), recipients=[email_id])
+        msg.subject = "Flight Search Results"
+        msg.body = "Hello," ",\n\n"+results+" \n\nThank you for using our flight search service!"
+        mail.send(msg)
+        print("Email sent successfully")
+        # Render the results on the web page
         return render_template('index.html', results=results)
 
     return render_template('index.html')
